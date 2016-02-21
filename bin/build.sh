8658f8d28767cee6d2ff1d53630f531f459f22a9
ataylorme <andrew@ataylor.me>
2016-02-21 14:00:05 -0600

    First run at CircleCI build/deploy scripts

---
+++ bin/build.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+#
+# Build script
+#
+txtred=$(tput setaf 1) # Red
+txtgrn=$(tput setaf 2) # Green
+txtylw=$(tput setaf 3) # Yellow
+txtblu=$(tput setaf 4) # Blue
+txtpur=$(tput setaf 5) # Purple
+txtcyn=$(tput setaf 6) # Cyan
+txtwht=$(tput setaf 7) # White
+txtrst=$(tput sgr0) # Text reset.
+
+
+JSON=composer.json
+VENDOR=./vendor
+EXE=composer
+
+# CI options
+export COMPOSER_DISCARD_CHANGES=1
+export COMPOSER_NO_INTERACTION=1
+
+if [ ! -f "$JSON" ]
+then
+	echo ${txtred}Error: No $JSON found ${txtrst}
+	exit 1
+fi
+
+FOUND=`which $EXE`
+if [ ! -x "$FOUND" ]
+then
+	echo ${txtred}Error: executable composer not found on path ${txtrst}
+	exit 1
+fi
+
+COMPOSER_PARMS="--no-ansi --no-dev --no-interaction --optimize-autoloader --prefer-source"
+
+# Install Composer dependencies
+echo -e "\n${txtylw} Invoking: $FOUND install $COMPOSER_PARMS ${txtrst}"
+$FOUND install $COMPOSER_PARMS
+
+EXE=gulp
+
+FOUND=`which $EXE`
+if [ ! -x "$FOUND" ]
+then
+	echo ${txtred}Error: executable gulp not found on path ${txtrst}
+	exit 1
+fi
+
+# Look for gulpfile.js occurrences NOT in node_modules
+FILE=gulpfile.*js
+for d in `find . \( -name node_modules -or -name components \) -prune -o -name "$FILE" | grep "$FILE"`
+do
+	# Change into containing directory
+	echo -e "\n${txtylw}gulpfile found, changing directories into: ${d%/*} ${txtrst}"
+	cd ${d%/*}
+
+	# Install any dependencies, if we find packages.json
+	echo -e "\n${txtylw}package.json found, running 'npm install' ${txtrst}"
+	[ -f 'package.json' ] && npm install
+
+	# Run gulp
+	echo -e "\n${txtylw}Running 'gulp' ${txtrst}"
+	$FOUND
+
+	# Change back again
+	echo -e "\n${txtylw}changed directories back into: ${txtrst}"
+	cd -
+done
---
+++ bin/deploy_to_pantheon.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Variables
+BUILD_DIR=$(pwd)
+txtred=$(tput setaf 1) # Red
+txtgrn=$(tput setaf 2) # Green
+txtylw=$(tput setaf 3) # Yellow
+txtblu=$(tput setaf 4) # Blue
+txtpur=$(tput setaf 5) # Purple
+txtcyn=$(tput setaf 6) # Cyan
+txtwht=$(tput setaf 7) # White
+txtrst=$(tput sgr0) # Text reset.
+
+# Install Terminus
+echo -e "\n${txtylw}Installing Terminus ${txtrst}"
+sudo curl https://github.com/pantheon-systems/cli/releases/download/0.10.4/terminus.phar -L -o /usr/local/bin/terminus
+sudo chmod +x /usr/local/bin/terminus
+
+# Log into terminus.
+echo -e "\n${txtylw}Logging into Terminus ${txtrst}"
+terminus auth login --machine-token=$TERMINUS_MACHINE_TOKEN
+
+echo -e "\n${txtylw}Creating a backup of the dev environment for site $PANTHEON_SITE_UUID ${txtrst}"
+terminus site backups create --element=all --site=$PANTHEON_SITE_UUID --env=dev
+
+# Clone deployment repository.
+echo -e "\n${txtylw}Cloning $PANTHEON_CODE ${txtrst}"
+git clone $PANTHEON_CODE pantheon
+
+cd $HOME
+echo -e "\n${txtylw}Cloning Pantheon repository into $HOME/pantheon  ${txtrst}"
+git clone $PANTHEON_GIT_URL pantheon
+
+cd pantheon
+
+mkdir -p public
+
+echo -e "\n${txtylw}Rsyncing $BUILD_DIR/public ${txtrst}"
+rsync -a $BUILD_DIR/public/* .
+
+echo -e "\n${txtylw}Copying $BUILD_DIR/pantheon.yml ${txtrst}"
+cp $BUILD_DIR/pantheon.yml .
+
+echo -e "\n${txtylw}Forcibly adding all files and committing${txtrst}"
+git add -A --force .
+git commit -m "Circle CI build $CIRCLE_BUILD_NUM by $CIRCLE_PROJECT_USERNAME from commit CIRCLE_SHA1"
+
+echo -e "\n${txtylw}Pushing to pantheon  ${txtrst}"
+git push --force origin master

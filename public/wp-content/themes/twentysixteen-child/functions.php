c61184c8be6d06eab42fc5087dc91780e6d63ea1
ataylorme <andrew@ataylor.me>
2016-05-27 09:50:09 -0500

    Formatting updates; Correct BrowserSync version

--- public/wp-content/themes/twentysixteen-child/functions.php
+++ public/wp-content/themes/twentysixteen-child/functions.php
@@ -1,28 +1,26 @@
 <?php
 namespace twenty_sixteen_child_theme;
 
-function enqueue_scripts_styles()
-{
+function enqueue_scripts_styles() {
 
-    $js_deps = $css_deps = array();
-    $js_deps[] = 'jquery';
+	$js_deps   = $css_deps = array();
+	$js_deps[] = 'jquery';
 
-    wp_dequeue_style('twentysixteen-style');
-    wp_enqueue_style('twentysixteen-style', get_template_directory_uri() . '/style.css');
-    wp_enqueue_style('twentysixteen-child', get_stylesheet_directory_uri() . '/assets/css/twentysixteen-child.css');
+	wp_dequeue_style( 'twentysixteen-style' );
+	wp_enqueue_style( 'twentysixteen-style', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'twentysixteen-child', get_stylesheet_directory_uri() . '/assets/css/twentysixteen-child.css' );
 
-    wp_enqueue_script('twentysixteen-child', get_stylesheet_directory_uri() . '/assets/js/twentysixteen-child.js', $js_deps, false, true);
+	wp_enqueue_script( 'twentysixteen-child', get_stylesheet_directory_uri() . '/assets/js/twentysixteen-child.js', $js_deps, false, true );
 }
 
-add_action('wp_enqueue_scripts', __NAMESPACE__ . '\enqueue_scripts_styles');
+add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\enqueue_scripts_styles' );
 
-function add_browser_sync_snippet_to_footer()
-{
-    echo '<script type=\'text/javascript\' id="__bs_script__">//<![CDATA[' . PHP_EOL;
-    echo "\t" . 'document.write("<script async src=\'http://HOST:3000/browser-sync/browser-sync-client.2.11.1.js\'><\/script>".replace("HOST", location.hostname));' . PHP_EOL;
-    echo '//]]></script>' . PHP_EOL;
+function add_browser_sync_snippet_to_footer() {
+	echo '<script type=\'text/javascript\' id="__bs_script__">//<![CDATA[' . PHP_EOL;
+	echo "\t" . 'document.write("<script async src=\'http://HOST:3000/browser-sync/browser-sync-client.2.12.10.js\'><\/script>".replace("HOST", location.hostname));' . PHP_EOL;
+	echo '//]]></script>' . PHP_EOL;
 }
 
-if (IS_LOCAL) {
-    add_action('wp_footer', __NAMESPACE__ . '\add_browser_sync_snippet_to_footer');
+if ( IS_LOCAL ) {
+	add_action( 'wp_footer', __NAMESPACE__ . '\add_browser_sync_snippet_to_footer' );
 }

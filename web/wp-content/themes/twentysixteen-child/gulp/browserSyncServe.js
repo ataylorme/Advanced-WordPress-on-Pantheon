a29b12e48f2f805f31d485fba6156d416ad05f31
ataylorme <andrew@ataylor.me>
2016-05-27 11:57:58 -0500

    Don't start a BrowserSync server

--- web/wp-content/themes/twentysixteen-child/gulp/browserSyncServe.js
+++ web/wp-content/themes/twentysixteen-child/gulp/browserSyncServe.js
@@ -3,10 +3,6 @@ import {browserSync} from './constants';
 export default () => {
     browserSync.init({
         // see http://www.browsersync.io/docs/options/
-        open: false,
-        server: {
-            baseDir: '.',
-            index: 'index.html'
-        }
+        open: false
     });
 }

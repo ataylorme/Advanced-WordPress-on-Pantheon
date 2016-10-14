bcdb8d20832950e215a855476ac14e144387bb6c
ataylorme <andrew@ataylor.me>
2016-10-14 15:36:20 -0500

    Don't ignore wp-config

---
+++ web/wp-config.php
@@ -0,0 +1,201 @@
+<?php
+
+/**
+ * Set root path
+ */
+$rootPath = realpath( __DIR__ . '/..' );
+
+/**
+ * Include the Composer autoload
+ */
+require_once( $rootPath . '/vendor/autoload.php' );
+
+/*
+ * Fetch .env
+ */
+if ( file_exists( $rootPath . '/.env' ) ) {
+	$dotenv = new Dotenv\Dotenv( $rootPath );
+	$dotenv->load();
+	$dotenv->required( array(
+		'DB_NAME',
+		'DB_USER',
+		'DB_HOST',
+	) )->notEmpty();
+}
+
+if ( isset( $_SERVER['HTTP_HOST'] ) ) {
+	// HTTP is still the default scheme for now.
+	$scheme = 'http';
+
+	/**
+	 * If we have detected that the end use is HTTPS, make sure we pass that
+	 * through here, so <img> tags and the like don't generate mixed-mode
+	 * content warnings.
+	 */
+	if ( isset( $_SERVER['HTTP_USER_AGENT_HTTPS'] ) && $_SERVER['HTTP_USER_AGENT_HTTPS'] == 'ON' ) {
+		$scheme = 'https';
+	}
+
+	/**
+	 * Define site and home URLs
+	 */
+	define( 'WP_HOME', getenv( 'WP_HOME' ) !== false ? getenv( 'WP_HOME' ) : $scheme . '://' . $_SERVER['HTTP_HOST'] );
+	define( 'WP_SITEURL', getenv( 'WP_SITEURL' ) !== false ? getenv( 'WP_SITEURL' ) : $scheme . '://' . $_SERVER['HTTP_HOST'] . '/wp' );
+}
+
+/**
+ * Limit post revisions to 3
+ */
+define( 'WP_POST_REVISIONS', 3 );
+
+/**
+ * Disallow on server file edits
+ */
+define( 'DISALLOW_FILE_EDIT', true );
+define( 'DISALLOW_FILE_MODS', true );
+
+/**
+ * Don't show deprecations
+ */
+error_reporting( E_ALL ^ E_DEPRECATED );
+
+/**
+ * WordPress Database Table prefix
+ * Use something other than `wp_` for security
+ */
+$table_prefix = getenv( 'DB_PREFIX' ) !== false ? getenv( 'DB_PREFIX' ) : 'wp_';
+
+/**
+ * Only include if not on a Pantheon environment
+ */
+if ( ! isset( $_ENV['PANTHEON_ENVIRONMENT'] ) ):
+
+	/**
+	 * Set Database Details
+	 */
+	define( 'DB_NAME', getenv( 'DB_NAME' ) );
+	define( 'DB_USER', getenv( 'DB_USER' ) );
+	define( 'DB_PASSWORD', getenv( 'DB_PASSWORD' ) !== false ? getenv( 'DB_PASSWORD' ) : '' );
+	define( 'DB_HOST', getenv( 'DB_HOST' ) );
+
+	/**
+	 * Set debug modes
+	 */
+	define( 'WP_DEBUG', getenv( 'WP_DEBUG' ) === 'true' ? true : false );
+	define( 'IS_LOCAL', getenv( 'IS_LOCAL' ) !== false ? true : false );
+
+	/**#@+
+	 * Authentication Unique Keys and Salts.
+	 *
+	 * Change these to different unique phrases!
+	 * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+	 * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+	 *
+	 * @since 2.6.0
+	 */
+	define( 'AUTH_KEY', '^J/U[^{cOJxhcmmCq2MX%nUs}i_^7nm[w+VsetLZ[JXW9Un/IiyWVEXk;s}X=?u$' );
+	define( 'SECURE_AUTH_KEY', 'dT,wlW20L5V3ChmTEHFGVtUE-r&A)y+G%Pnql&eKdVAWvdIr9FO4lh_Gc9ZVn!1|' );
+	define( 'LOGGED_IN_KEY', '{0E{}k_e7!XRt*}h}nuMP[sKn$gb(O@|[>?bUs}B{>:|+|lL%czE/!Tlc Uk53#:' );
+	define( 'NONCE_KEY', '|M9$H1t9D@AR6>JM[]?9RoA^dmOCHt6ldAE%x|0 Iqpi+m32>1>?0*_?*#|6f7|W' );
+	define( 'AUTH_SALT', 'CA-BmAsS|o_P|!I8Wfu%a=qXC;!3p[8]W_:N2{oI]HhpLP(%2]zWLH+aHTHDw9>%' );
+	define( 'SECURE_AUTH_SALT', 'pi-EA,AOXk*U[VZ|t]R;@K<WMcbD)>k* ;8+hKX:A|$.Z@HL@0`SE?W0:-?-IRd!' );
+	define( 'LOGGED_IN_SALT', 'e+6%u)u@RZn-$}_Q[N;Na<|A-[Am_$#nhD~}ci:%R&B*oiq<sPF$v)d1r<-V-5W|' );
+	define( 'NONCE_SALT', 'r%oyx_`[A-~<LB)]I.,^//}/&]a)H|fzk3IUWrZn[L4qf#Pp#lsB-B}+/ai&u,/|' );
+
+endif;
+
+/**
+ * Begin Pantheon wp-config.php settings
+ *
+ *         .+?:
+ *          .+??.
+ *            ??? .
+ *            +???.
+ *       +?????????=.
+ *       .???????????.
+ *       .????????????.
+ *
+ *      ########### ########
+ *      ############.#######.
+ *      ####### ####  .......
+ *      ######## #### #######
+ *      #########.####.######
+ *      ######  ...
+ *      #######.??.##########
+ *      #######~+??.#########
+ *      ########.??..
+ *      #########.??.#######.
+ *      #########.+?? ######.
+ *                .+?.
+ *          .????????????.
+ *            +??????????,
+ *             .????++++++.
+ *               ????.
+ *               .???,
+ *                .~??.
+ *                  .??
+ *                   .?,.
+ */
+
+if ( isset( $_ENV['PANTHEON_ENVIRONMENT'] ) ):
+	// ** MySQL settings - included in the Pantheon Environment ** //
+	/** The name of the database for WordPress */
+	define( 'DB_NAME', $_ENV['DB_NAME'] );
+
+	/** MySQL database username */
+	define( 'DB_USER', $_ENV['DB_USER'] );
+
+	/** MySQL database password */
+	define( 'DB_PASSWORD', $_ENV['DB_PASSWORD'] );
+
+	/** MySQL hostname; on Pantheon this includes a specific port number. */
+	define( 'DB_HOST', $_ENV['DB_HOST'] . ':' . $_ENV['DB_PORT'] );
+
+	/** Database Charset to use in creating database tables. */
+	define( 'DB_CHARSET', 'utf8' );
+
+	/** The Database Collate type. Don't change this if in doubt. */
+	define( 'DB_COLLATE', '' );
+
+	/**#@+
+	 * Authentication Unique Keys and Salts.
+	 *
+	 * Change these to different unique phrases!
+	 * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+	 * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+	 *
+	 * Pantheon sets these values for you also. If you want to shuffle them you
+	 * can do so via your dashboard.
+	 *
+	 * @since 2.6.0
+	 */
+	define( 'AUTH_KEY', $_ENV['AUTH_KEY'] );
+	define( 'SECURE_AUTH_KEY', $_ENV['SECURE_AUTH_KEY'] );
+	define( 'LOGGED_IN_KEY', $_ENV['LOGGED_IN_KEY'] );
+	define( 'NONCE_KEY', $_ENV['NONCE_KEY'] );
+	define( 'AUTH_SALT', $_ENV['AUTH_SALT'] );
+	define( 'SECURE_AUTH_SALT', $_ENV['SECURE_AUTH_SALT'] );
+	define( 'LOGGED_IN_SALT', $_ENV['LOGGED_IN_SALT'] );
+	define( 'NONCE_SALT', $_ENV['NONCE_SALT'] );
+	/**#@-*/
+
+	// Force the use of a safe temp directory when in a container
+	if ( defined( 'PANTHEON_BINDING' ) ):
+		define( 'WP_TEMP_DIR', sprintf( '/srv/bindings/%s/tmp', PANTHEON_BINDING ) );
+	endif;
+endif;
+
+/*
+ * Define wp-content directory outside of WordPress directory
+ */
+define( 'WP_CONTENT_DIR', dirname( __FILE__ ) . '/wp-content' );
+define( 'WP_CONTENT_URL', getenv( 'WP_CONTENT_URL' ) !== false ? getenv( 'WP_CONTENT_URL' ) : WP_HOME . '/wp-content' );
+
+/* That's all, stop editing! Happy Pressing. */
+
+/** Absolute path to the WordPress directory. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');

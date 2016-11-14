78f9e2e4d466770d3a84753df6565255be6e7cea
ataylorme <andrew@ataylor.me>
2016-11-14 15:46:56 -0600

    Use minified CSS/JS

--- web/wp-content/themes/twentysixteen-child/functions.php
+++ web/wp-content/themes/twentysixteen-child/functions.php
@@ -8,9 +8,9 @@ function enqueue_scripts_styles() {
 
 	wp_dequeue_style( 'twentysixteen-style' );
 	wp_enqueue_style( 'twentysixteen-style', get_template_directory_uri() . '/style.css' );
-	wp_enqueue_style( 'twentysixteen-child', get_stylesheet_directory_uri() . '/assets/css/twentysixteen-child.css' );
+	wp_enqueue_style( 'twentysixteen-child', get_stylesheet_directory_uri() . '/assets/css/twentysixteen-child.min.css' );
 
-	wp_enqueue_script( 'twentysixteen-child', get_stylesheet_directory_uri() . '/assets/js/twentysixteen-child.js', $js_deps, false, true );
+	wp_enqueue_script( 'twentysixteen-child', get_stylesheet_directory_uri() . '/assets/js/twentysixteen-child.min.js', $js_deps, false, true );
 }
 
 add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\enqueue_scripts_styles' );

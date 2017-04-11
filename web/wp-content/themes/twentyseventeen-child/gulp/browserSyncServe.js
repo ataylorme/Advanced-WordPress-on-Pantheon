a9d60077d4aa3660fe6745254d573b2d54996657
ataylorme <andrew@ataylor.me>
2017-04-11 14:07:37 -0500

    Update BrowserSync for working with Kalabox

--- web/wp-content/themes/twentyseventeen-child/gulp/browserSyncServe.js
+++ web/wp-content/themes/twentyseventeen-child/gulp/browserSyncServe.js
@@ -5,7 +5,8 @@ export default () => {
         // see http://www.browsersync.io/docs/options/
         open: false,
         https: true,
-        proxy: 'https://advanced-wp.dev/',
-		files: ['assets/css/*.min.css', 'assets/js/*.min.js', '**/*.php']
+        proxy: 'https://pantheon-wp-best-practices.kbox.site/',
+		files: ['assets/css/*.min.css', 'assets/js/*.min.js', '**/*.php'],
+		reloadDelay: 1500
     });
 }

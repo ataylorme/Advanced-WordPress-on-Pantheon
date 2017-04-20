23fd4ef80b5d5673e40c3c3664338c9673d3401e
ataylorme <andrew@ataylor.me>
2017-04-20 10:32:32 -0500

    Add link to multidev back to PR; Only create a multidev if we have a PR

--- bin/deploy_to_pantheon.sh
+++ bin/deploy_to_pantheon.sh
@@ -13,6 +13,7 @@ txtrst=$(tput sgr0) # Text reset.
 
 COMMIT_MESSAGE="$(git show --name-only --decorate)"
 PANTHEON_ENV="dev"
+GITHUB_API_URL="https://api.github.com/repos/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME"
 
 cd $HOME
 
@@ -40,10 +41,13 @@ git fetch
 echo -e "\n${txtylw}Logging into Terminus ${txtrst}"
 terminus auth:login --machine-token=$PANTHEON_MACHINE_TOKEN
 
+# Stash site name
+PANTHEON_SITE_NAME="$(terminus site:info $PANTHEON_SITE_UUID --fields=name --format=string)"
+
 SLACK_MESSAGE="Circle CI build ${CIRCLE_BUILD_NUM} by ${CIRCLE_PROJECT_USERNAME} was successful and has been deployed to Pantheon on <https://dashboard.pantheon.io/sites/${PANTHEON_SITE_UUID}#dev/code|the dev environment>! \nTo deploy to test run "'`terminus env:deploy '"${PANTHEON_SITE_UUID}"'.test`'" or merge from <https://dashboard.pantheon.io/sites/${PANTHEON_SITE_UUID}#test/deploys|the site dashboard>."
 
-# Check if we are NOT on the master branch
-if [ $CIRCLE_BRANCH != "master" ]
+# Check if we are NOT on the master branch and this is a PR
+if [ $CIRCLE_BRANCH != "master" && && -n "${CIRCLE_PR_NUMBER}" ]
 then
 
 	# Branch name can't be more than 11 characters
@@ -90,6 +94,12 @@ then
 		# otherwise, create the multidev branch
 		echo -e "\n${txtylw}Multidev not found, creating the multidev branch ${normalize_branch} via Terminus ${txtrst}"
 		terminus multidev:create $PANTHEON_SITE_UUID.dev $normalize_branch
+
+		# put a link to the multidev back on GitHub
+		echo -e "\n${txtylw}Linking multidev back to PR #$CIRCLE_PR_NUMBER ${txtrst}"
+		MULTDEV_LINK="http://$normalize_branch-$PANTHEON_SITE_NAME.pantheonsite.io/"
+		curl -i -u "$GIT_USERNAME:$GIT_TOKEN" -d "{\"body\": \"Multidev `$normalize_branch` created successfully! [$MULTDEV_LINK]($MULTDEV_LINK)\"}" $GITHUB_API_URL/issues/$CIRCLE_PR_NUMBER/comments
+
 		git fetch
 	fi
 

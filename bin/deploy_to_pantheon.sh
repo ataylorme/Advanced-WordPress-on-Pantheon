b4dbd26f9331119adbf3316084bdf037fd9c956f
ataylorme <andrew@ataylor.me>
2017-04-20 11:01:38 -0500

    Test CI_PULL_REQUEST

--- bin/deploy_to_pantheon.sh
+++ bin/deploy_to_pantheon.sh
@@ -46,9 +46,9 @@ PANTHEON_SITE_NAME="$(terminus site:info $PANTHEON_SITE_UUID --fields=name --for
 
 SLACK_MESSAGE="Circle CI build ${CIRCLE_BUILD_NUM} by ${CIRCLE_PROJECT_USERNAME} was successful and has been deployed to Pantheon on <https://dashboard.pantheon.io/sites/${PANTHEON_SITE_UUID}#dev/code|the dev environment>! \nTo deploy to test run "'`terminus env:deploy '"${PANTHEON_SITE_UUID}"'.test`'" or merge from <https://dashboard.pantheon.io/sites/${PANTHEON_SITE_UUID}#test/deploys|the site dashboard>."
 
-if [ -n "$CIRCLE_PULL_REQUEST" ]
+if [ -n "$CI_PULL_REQUEST" ]
 then
-	echo -e "CIRCLE_PULL_REQUEST is: $CIRCLE_PULL_REQUEST"
+	echo -e "CIRCLE_PULL_REQUEST is: $CI_PULL_REQUEST"
 else
 	echo -e "CIRCLE_PULL_REQUEST is NOT set or is empty"
 fi
@@ -56,10 +56,10 @@ fi
 exit 1
 
 # Check if we are NOT on the master branch and this is a PR
-if [[ $CIRCLE_BRANCH != "master" && -n "$CIRCLE_PULL_REQUEST" ]]
+if [[ $CIRCLE_BRANCH != "master" && -n "$CI_PULL_REQUEST" ]]
 then
 	# Stash PR number
-	PR_NUMBER=${CIRCLE_PULL_REQUEST##*/}
+	PR_NUMBER=${CI_PULL_REQUEST##*/}
 
 	# Branch name can't be more than 11 characters
 	# Normalize branch name to adhere with Multidev requirements

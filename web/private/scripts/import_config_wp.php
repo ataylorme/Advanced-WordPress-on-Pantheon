972622bf45395a51a6198cf52d2a23bba5eb09e9
ataylorme <andrew@ataylor.me>
2017-04-21 16:50:51 -0500

    Don't auto import pantheon_dev/pantheon_test/pantheon_live bundles via WP-CFM

--- web/private/scripts/import_config_wp.php
+++ web/private/scripts/import_config_wp.php
@@ -16,30 +16,6 @@ $slack_user_icon    = $secrets['live_url'] . '/wp-content/uploads/icons/wp-cfm.p
 exec( 'wp plugin activate wp-cfm 2>&1' );
 
 // Automagically import config into WP-CFM site upon code deployment
-
-$config_map  = array(
-	'test'    => 'pantheon_live',
-	'test'    => 'pantheon_test',
-	'dev'     => 'pantheon_dev',
-	'default' => 'pantheon_dev'
-);
-$config_name = array_key_exists( PANTHEON_ENVIRONMENT, $config_map ) ? $config_map[ PANTHEON_ENVIRONMENT ] : $config_map['default'];
-
-_slack_tell( 'Importation of WordPress WP-CFM Default Configuration on the ' . PANTHEON_ENVIRONMENT . ' environment is starting...', $slack_channel_name, $slack_user_name, $slack_user_icon );
-
-exec( 'wp config pull ' . $config_name . ' 2>&1', $output );
-if ( count( $output ) > 0 ) {
-	$output = preg_replace( '/\s+/', ' ', array_slice( $output, 1, - 1 ) );
-	$output = str_replace( ' update', ' [update]', $output );
-	$output = str_replace( ' create', ' [create]', $output );
-	$output = str_replace( ' delete', ' [delete]', $output );
-	$output = implode( $output, "\n" );
-	$output = rtrim( $output );
-	_slack_tell( $output, $slack_channel_name, $slack_user_name, $slack_user_icon, '#A9A9A9' );
-}
-
-_slack_tell( 'Importation of WordPress WP-CFM Default Configuration on the ' . PANTHEON_ENVIRONMENT . ' environment is complete.', $slack_channel_name, $slack_user_name, $slack_user_icon );
-
 $path  = $_SERVER['DOCUMENT_ROOT'] . '/private/config';
 $files = scandir( $path );
 $files = array_diff( scandir( $path ), array( '.', '..' ) );

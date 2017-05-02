7e09b53270b06a9efbb70b85ac62f937cb7c9142
ataylorme <andrew@ataylor.me>
2017-05-02 10:33:31 -0500

    Check Pantheon user UUID to confirm Terminus authentication

--- bin/behat-prepare.sh
+++ bin/behat-prepare.sh
@@ -10,8 +10,9 @@
 echo -e "\nLogging into Terminus"
 terminus auth:login --machine-token=$PANTHEON_MACHINE_TOKEN
 
-terminus auth:whoami > /dev/null
-if [ $? -ne 0 ]; then
+# Check if logged into Terminus
+TERMINUS_USER_ID=$(terminus auth:whoami --field=id 2>&1)
+if [[ ! $TERMINUS_USER_ID =~ ^[A-Za-z0-9-]{36}$ ]]; then
 	echo "Terminus unauthenticated; assuming unauthenticated build"
 	exit 0
 fi
--- bin/deploy_to_pantheon.sh
+++ bin/deploy_to_pantheon.sh
@@ -15,6 +15,13 @@ txtrst=$(tput sgr0) # Text reset.
 echo -e "\n${txtylw}Logging into Terminus ${txtrst}"
 terminus auth:login --machine-token=$PANTHEON_MACHINE_TOKEN
 
+# Check if logged into Terminus
+TERMINUS_USER_ID=$(terminus auth:whoami --field=id 2>&1)
+if [[ ! $TERMINUS_USER_ID =~ ^[A-Za-z0-9-]{36}$ ]]; then
+	echo "Terminus unauthenticated; assuming unauthenticated build"
+	exit 0
+fi
+
 # Set variables
 COMMIT_MESSAGE="$(git show --name-only --decorate)"
 PANTHEON_ENVS="$(terminus multidev:list $PANTHEON_SITE_UUID --format=list --field=Name)"

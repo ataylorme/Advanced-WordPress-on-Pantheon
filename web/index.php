16453fd24018a87335bd45c394efc1d7bf189b19
ataylorme <andrew@ataylor.me>
2017-05-12 18:47:18 -0500

    Cleanup WPCS errors

--- web/index.php
+++ web/index.php
@@ -1,6 +1,16 @@
 <?php
-// Tells WordPress to load the WordPress theme and output it.
+/**
+ * Advanced WordPress on Pantheon index
+ *
+ * @package AdvancedWordPressOnPantheon
+ */
+
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ */
 define( 'WP_USE_THEMES', true );
 
-// Loads the WordPress Environment and Template
+/**
+ * Loads the WordPress Environment and Template
+ */
 require( dirname( __FILE__ ) . '/wp/wp-blog-header.php' );

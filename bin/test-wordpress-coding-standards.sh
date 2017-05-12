8b68b9b5a0edb9639f2988f246a9274e5ffc265d
ataylorme <andrew@ataylor.me>
2017-05-12 17:43:42 -0500

    Run the code sniff

--- bin/test-wordpress-coding-standards.sh
+++ bin/test-wordpress-coding-standards.sh
@@ -3,11 +3,14 @@
 # Add WordPress coding standards path to code sniffer
 phpcs --config-set installed_paths $HOME/.composer/vendor/wp-coding-standards/wpcs
 
+# Don't exit on warnings
+phpcs --config-set ignore_warnings_on_exit 1
+
 # Change to project directory
 cd $HOME/$CIRCLE_PROJECT_REPONAME
 
 # Sniff all PHP files
-phpcs --standard=phpcs.ruleset.xml --config-set ignore_warnings_on_exit 1 $(find . -name '*.php')
+phpcs --standard=phpcs.ruleset.xml $(find . -name '*.php')
 
 # Change directories back to wherever we were before
 cd -

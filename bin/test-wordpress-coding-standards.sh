0a6f88ea96513203ab115cc8ed72f5d9ac550902
ataylorme <andrew@ataylor.me>
2017-05-12 18:12:27 -0500

    find instead of loop

--- bin/test-wordpress-coding-standards.sh
+++ bin/test-wordpress-coding-standards.sh
@@ -3,22 +3,12 @@
 # Add WordPress coding standards path to code sniffer
 phpcs --config-set installed_paths $HOME/.composer/vendor/wp-coding-standards/wpcs
 
-# Assume no errors
-PHPCS_ERRORS=0
-
 # Change to project directory
 cd $HOME/$CIRCLE_PROJECT_REPONAME
 
 # Sniff all PHP files
-find . -name '*.php' -print0 | while IFS= read -r -d $'\0' phpfile; do
-    phpcs --standard=phpcs.ruleset.xml $phpfile
-	if [ "$?" != 0 ]
-	then
-		PHPCS_ERRORS=1
-	fi
-done
-
-if [ $PHPCS_ERRORS == 1 ]
+phpcs --standard=phpcs.ruleset.xml $(find . -name '*.php')
+if [ "$?" != 0 ]
 then
 	echo "\nPHPCS Failed. Please fix the error(s)."
 	exit 1

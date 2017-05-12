f34c53b2bf0a8c0adc14ea5d2c1d52819c353e75
ataylorme <andrew@ataylor.me>
2017-05-12 18:00:53 -0500

    Exit on phpcs error

--- bin/test-wordpress-coding-standards.sh
+++ bin/test-wordpress-coding-standards.sh
@@ -3,14 +3,26 @@
 # Add WordPress coding standards path to code sniffer
 phpcs --config-set installed_paths $HOME/.composer/vendor/wp-coding-standards/wpcs
 
-# Don't exit on warnings
-phpcs --config-set ignore_warnings_on_exit 1
+# Assume no errors
+PHPCS_ERRORS = 0
 
 # Change to project directory
 cd $HOME/$CIRCLE_PROJECT_REPONAME
 
 # Sniff all PHP files
-phpcs --standard=phpcs.ruleset.xml $(find . -name '*.php')
+find . -name '*.php' -print0 | while IFS= read -r -d $'\0' phpfile; do
+    phpcs --standard=phpcs.ruleset.xml phpfile
+	if [ "$?" != 0 ]
+	then
+		PHPCS_ERRORS = 0
+	fi
+done
+
+if [ $PHPCS_ERRORS == 1 ]
+then
+	echo "\nPHPCS Failed. Please fix the error(s)."
+	exit 1
+fi
 
 # Change directories back to wherever we were before
 cd -

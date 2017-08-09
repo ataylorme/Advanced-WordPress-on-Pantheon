3bd278c71e2dc397affba51884b986e54e8ed978
ataylorme <andrew@ataylor.me>
2017-08-09 08:46:13 -0500

    Only install Node packages when needed, rather than globally

--- .circleci/set-up-globals.sh
+++ .circleci/set-up-globals.sh
@@ -9,13 +9,6 @@ set -ex
 # Update current apt packages
 apt-get update
 
-# Install node, npm and yarn
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-apt-get install -y nodejs
-
-# Install gulp globally
-npm install --global gulp
-
 #=========================================================================
 # Commands below this line would not be transferable to a docker container
 #=========================================================================

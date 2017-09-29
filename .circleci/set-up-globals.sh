3e6d08105388581aefae3b8b80ee3d2eae85bbf2
ataylorme <andrew@ataylor.me>
2017-09-28 19:14:04 -0700

    No need to install Terminus or it's plugins, they're in the Dockerfile

--- .circleci/set-up-globals.sh
+++ .circleci/set-up-globals.sh
@@ -2,22 +2,9 @@
 
 set -ex
 
-#==================================================
-# The section would be transferable to a DOCKERFILE
-#==================================================
-
-# Update current apt packages
-apt-get update
-
 #=========================================================================
 # Commands below this line would not be transferable to a docker container
 #=========================================================================
-
-# Enable Composer parallel downloads
-composer global require -n "hirak/prestissimo:^0.3"
-
-# Install Terminus into ~/terminus
-/usr/bin/env COMPOSER_BIN_DIR=$HOME/bin composer --working-dir=$HOME require pantheon-systems/terminus "^1"
 
 #=====================================================================================================================
 # Start EXPORTing needed environment variables
@@ -54,25 +41,6 @@ source $BASH_ENV
 # End EXPORTing needed environment variables
 #===========================================
 
-#===============================
-# Start Install Terminus Plugins
-#===============================
-INSTALL_TERMINUS_PLUGINS() {
-	composer create-project -n -d $HOME/.terminus/plugins pantheon-systems/terminus-build-tools-plugin:$BUILD_TOOLS_VERSION
-	composer create-project -n -d $HOME/.terminus/plugins pantheon-systems/terminus-secrets-plugin:^1
-}
-
-# Create Terminus plugins directory and install plugins if needed
-if [ ! -d $HOME/.terminus/plugins ]
-then
-	mkdir -p $HOME/.terminus/plugins
-	INSTALL_TERMINUS_PLUGINS
-fi
-
-#===============================
-# End Install Terminus Plugins
-#===============================
-
 # Add a Git token for Composer
 if [ -n "$GITHUB_TOKEN" ] ; then
   composer config --global github-oauth.github.com $GITHUB_TOKEN
@@ -88,6 +56,10 @@ terminus --version
 terminus auth:login -n --machine-token="$TERMINUS_TOKEN"
 
 # Disable host checking
+if [ ! -d $HOME/.ssh ]
+then
+	mkdir -p $HOME/.ssh
+fi
 touch $HOME/.ssh/config
 echo "StrictHostKeyChecking no" >> "$HOME/.ssh/config"
 

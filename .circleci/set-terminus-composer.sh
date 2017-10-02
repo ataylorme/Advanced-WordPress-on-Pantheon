db054e453446a8cf94a3c39d93c663d8345a398e
ataylorme <andrew@ataylor.me>
2017-10-02 10:31:05 -0700

    Split Terminus and Composer configuration into a separate file from environment varaibles

---
+++ .circleci/set-terminus-composer.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -ex
+
+#=========================================================================
+# Commands below this line would not be transferable to a docker container
+#=========================================================================
+
+if [ -n "$GITHUB_TOKEN" ] ; then
+  composer config --global github-oauth.github.com $GITHUB_TOKEN
+fi
+
+# Bail on errors
+set +ex
+
+# Make sure Terminus is installed
+terminus --version
+
+# Authenticate with Terminus
+terminus auth:login -n --machine-token="$TERMINUS_TOKEN"
--- .circleci/set-up-globals.sh
+++ .circleci/set-up-globals.sh
@@ -41,19 +41,8 @@ source $BASH_ENV
 # End EXPORTing needed environment variables
 #===========================================
 
-# Add a Git token for Composer
-if [ -n "$GITHUB_TOKEN" ] ; then
-  composer config --global github-oauth.github.com $GITHUB_TOKEN
-fi
-
 # Bail on errors
 set +ex
-
-# Make sure Terminus is installed
-terminus --version
-
-# Authenticate with Terminus
-terminus auth:login -n --machine-token="$TERMINUS_TOKEN"
 
 # Disable host checking
 if [ ! -d $HOME/.ssh ]

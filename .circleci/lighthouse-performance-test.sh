5fde7498630c31b70248d4b06822db87eac8005f
ataylorme <andrew@ataylor.me>
2017-12-06 10:32:45 -0800

    Just run the Lighthouse report for live fresh instead of trying to fetch it from artifacts

--- .circleci/lighthouse-performance-test.sh
+++ .circleci/lighthouse-performance-test.sh
@@ -38,7 +38,9 @@ LIGHTHOUSE_REPORT_NAME="$LIGHTHOUSE_RESULTS_DIR/lighthouse.json"
 LIGHTHOUSE_JSON_REPORT="$LIGHTHOUSE_RESULTS_DIR/lighthouse.report.json"
 LIGHTHOUSE_HTML_REPORT="$LIGHTHOUSE_RESULTS_DIR/lighthouse.report.html"
 LIGHTHOUSE_RESULTS_JSON="$LIGHTHOUSE_RESULTS_DIR/lighthouse.results.json"
-LIGHTHOUSE_RESULTS_JSON_MASTER="lighthouse_results/master/lighthouse.results.json"
+LIGHTHOUSE_MASTER_HTML_REPORT="lighthouse_results/master/lighthouse.report.html"
+LIGHTHOUSE_MASTER_REPORT_NAME="lighthouse_results/master/lighthouse.report.json"
+LIGHTHOUSE_MASTER_RESULTS_JSON="lighthouse_results/master/lighthouse.results.json"
 
 # Delete the Lighthouse results directory so we don't keep old results around
 if [ -d "$LIGHTHOUSE_RESULTS_DIR" ]; then
@@ -47,17 +49,18 @@ fi
 
 # Create the Lighthouse results directory if it doesn't exist or has been deleted
 mkdir -p $LIGHTHOUSE_RESULTS_DIR
+if [ ! -d "lighthouse_results/master" ]; then
+	mkdir -p "lighthouse_results/master"
+fi
 
 # Stash Circle Artifacts URL
 CIRCLE_ARTIFACTS_URL="$CIRCLE_BUILD_URL/artifacts/$CIRCLE_NODE_INDEX/$CIRCLE_ARTIFACTS"
 
 # Ping the Pantheon environment to wake it from sleep and prime the cache
 echo -e "\nPinging the ${LIGHTHOUSE_BRANCH} environment to wake it from sleep..."
-curl -I "$LIGHTHOUSE_URL" >/dev/null
+curl -s -I "$LIGHTHOUSE_URL" >/dev/null
 
 # Run the Lighthouse test
-echo -e "\nRunning lighthouse --perf --save-artifacts --output json --output html --output-path ${LIGHTHOUSE_REPORT_NAME} --chrome-flags=\"--headless --disable-gpu --no-sandbox\" ${LIGHTHOUSE_URL}..."
-
 lighthouse --perf --save-artifacts --output json --output html --output-path ${LIGHTHOUSE_REPORT_NAME} --chrome-flags="--headless --disable-gpu --no-sandbox" ${LIGHTHOUSE_URL}
 
 # Check for HTML report file
@@ -75,61 +78,55 @@ fi
 # Create tailored results JSON file
 cat $LIGHTHOUSE_JSON_REPORT | jq '. | { "total-score": .score, "speed-index": .audits["speed-index-metric"]["score"], "first-meaningful-paint": .audits["first-meaningful-paint"]["score"], "estimated-input-latency": .audits["estimated-input-latency"]["score"], "time-to-first-byte": .audits["time-to-first-byte"]["rawValue"], "first-interactive": .audits["first-interactive"]["score"], "consistently-interactive": .audits["consistently-interactive"]["score"], "critical-request-chains": .audits["critical-request-chains"]["displayValue"], "redirects": .audits["redirects"]["score"], "bootup-time": .audits["bootup-time"]["rawValue"], "uses-long-cache-ttl": .audits["uses-long-cache-ttl"]["score"], "total-byte-weight": .audits["total-byte-weight"]["score"], "offscreen-images": .audits["offscreen-images"]["score"], "uses-webp-images": .audits["uses-webp-images"]["score"], "uses-optimized-images": .audits["uses-optimized-images"]["score"], "uses-request-compression": .audits["uses-request-compression"]["score"], "uses-responsive-images": .audits["uses-responsive-images"]["score"], "dom-size": .audits["dom-size"]["score"], "script-blocking-first-paint": .audits["script-blocking-first-paint"]["score"] }' > $LIGHTHOUSE_RESULTS_JSON
 
+LIGHTHOUSE_SCORE=$(cat $LIGHTHOUSE_RESULTS_JSON | jq '.["total-score"] | floor | tonumber')
+LIGHTHOUSE_HTML_REPORT_URL="$CIRCLE_ARTIFACTS_URL/$LIGHTHOUSE_HTML_REPORT"
+
 # Rsync files to CIRCLE_ARTIFACTS_DIR
 echo -e "\nRsyincing lighthouse_results files to $CIRCLE_ARTIFACTS_DIR..."
 rsync -rlvz lighthouse_results $CIRCLE_ARTIFACTS_DIR
 
-LIGHTHOUSE_SCORE=$(cat $LIGHTHOUSE_RESULTS_JSON | jq '.["total-score"] | floor | tonumber')
-LIGHTHOUSE_RESULTS=$(cat $LIGHTHOUSE_RESULTS_JSON | jq '.|tostring')
-LIGHTHOUSE_HTML_REPORT_URL="$CIRCLE_ARTIFACTS_URL/$LIGHTHOUSE_HTML_REPORT"
-REPORT_LINK="[Lighthouse performance report]($LIGHTHOUSE_HTML_REPORT_URL)"
+# If we are on on the master branch
+if [[ ${CIRCLE_BRANCH} == "master" ]]; then
+	echo -e "\nLighthouse score is $LIGHTHOUSE_SCORE"
+	exit 0
+fi
 
-# Crawl Circle CI API to get the latest results from artifacts stored in build from the master branch
-ARRAY_KEY=0
+# Ping the live environment to wake it from sleep and prime the cache
+echo -e "\nPinging the live environment to wake it from sleep..."
+curl -s -I "$LIVE_SITE_URL" >/dev/null
 
-while [ -z $LAST_SUCCESSFUL_MASTER_BUILD_RESULT_JSON_URL ]
-do
-	LAST_SUCCESSFUL_MASTER_BUILD_NUM=$(curl -s https://circleci.com/api/v1.1/project/github/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/tree/master | jq ".[$ARRAY_KEY].previous_successful_build.build_num | tonumber")
+# Run Lighthouse on the live environment
+echo -e "\nRunning Lighthouse on the live environment"
+lighthouse --perf --save-artifacts --output json --output html --output-path "$LIGHTHOUSE_MASTER_REPORT_NAME" --chrome-flags="--headless --disable-gpu --no-sandbox" ${LIVE_SITE_URL}
+
+# Create tailored results JSON file
+cat $LIGHTHOUSE_MASTER_REPORT_NAME | jq '. | { "total-score": .score, "speed-index": .audits["speed-index-metric"]["score"], "first-meaningful-paint": .audits["first-meaningful-paint"]["score"], "estimated-input-latency": .audits["estimated-input-latency"]["score"], "time-to-first-byte": .audits["time-to-first-byte"]["rawValue"], "first-interactive": .audits["first-interactive"]["score"], "consistently-interactive": .audits["consistently-interactive"]["score"], "critical-request-chains": .audits["critical-request-chains"]["displayValue"], "redirects": .audits["redirects"]["score"], "bootup-time": .audits["bootup-time"]["rawValue"], "uses-long-cache-ttl": .audits["uses-long-cache-ttl"]["score"], "total-byte-weight": .audits["total-byte-weight"]["score"], "offscreen-images": .audits["offscreen-images"]["score"], "uses-webp-images": .audits["uses-webp-images"]["score"], "uses-optimized-images": .audits["uses-optimized-images"]["score"], "uses-request-compression": .audits["uses-request-compression"]["score"], "uses-responsive-images": .audits["uses-responsive-images"]["score"], "dom-size": .audits["dom-size"]["score"], "script-blocking-first-paint": .audits["script-blocking-first-paint"]["score"] }' > $LIGHTHOUSE_MASTER_RESULTS_JSON
+
+LIGHTHOUSE_MASTER_SCORE="$(cat $LIGHTHOUSE_MASTER_RESULTS_JSON | jq \".[\"total-score\"]  | floor | tonumber\")"
+
+# Rsync files to CIRCLE_ARTIFACTS_DIR again now that we have master results
+echo -e "\nRsyincing lighthouse_results files to $CIRCLE_ARTIFACTS_DIR..."
+rsync -rlvz lighthouse_results $CIRCLE_ARTIFACTS_DIR
+
+echo -e "\nMaster score of $LIGHTHOUSE_MASTER_SCORE recorded"
+
+LIGHTHOUSE_MASTER_HTML_REPORT_URL="$CIRCLE_ARTIFACTS_URL/$LIGHTHOUSE_MASTER_HTML_REPORT"
+REPORT_LINK="[Lighthouse performance report for `$CIRCLE_BRANCH`]($LIGHTHOUSE_HTML_REPORT_URL) and compare it to the [Lighthouse performance report for the `master` branch]($LIGHTHOUSE_MASTER_HTML_REPORT_URL)"
 	
-	LAST_SUCCESSFUL_MASTER_BUILD_RESULT_JSON_URL=$(curl -s https://circleci.com/api/v1.1/project/github/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/$LAST_SUCCESSFUL_MASTER_BUILD_NUM/artifacts | jq ".[] | select(.url | endswith(\"lighthouse_results/master/lighthouse.results.json\")) | .url")
-
-	if [ $ARRAY_KEY -gt 9 ]; then
-		break;
-	fi
-
-	ARRAY_KEY=$[$ARRAY_KEY+1]
-done
-
-echo -e "\nLast successful master build with artifacts: $LAST_SUCCESSFUL_MASTER_BUILD_NUM"
-echo -e "\nPulling Lighthouse results from $LAST_SUCCESSFUL_MASTER_BUILD_RESULT_JSON_URL"
-
-if [ -n $LAST_SUCCESSFUL_MASTER_BUILD_RESULT_JSON_URL ]; then
-	LIGHTHOUSE_MASTER_SCORE=$(curl -s $LAST_SUCCESSFUL_MASTER_BUILD_RESULT_JSON_URL | jq ".[\"total-score\"]  | floor")
-	echo -e "\ncurl -s $LAST_SUCCESSFUL_MASTER_BUILD_RESULT_JSON_URL | jq \".[\\\"total-score\\\"]  | floor\""
-	echo -e "\n Stored master score of $LIGHTHOUSE_MASTER_SCORE found"
-	
-	if [ "$LIGHTHOUSE_SCORE" -lt "${LIGHTHOUSE_MASTER_SCORE:-90}" ]; then
-		# Lighthouse test failed! The score is less than the previous result on the master branch
-		echo -e "\nLighthouse test failed! The score of $LIGHTHOUSE_SCORE is less than the previous score of $LIGHTHOUSE_MASTER_SCORE on the master branch"
-		PR_MESSAGE="Lighthouse test failed! The score of \`$LIGHTHOUSE_SCORE\` is less than the previous score of \`$LIGHTHOUSE_MASTER_SCORE\` on the master branch."
-		exit 1
-	else
-		# Lighthouse test passed! The score isn't less than the previous result on the master branch
-		echo -e "\nLighthouse test passed! The score of $LIGHTHOUSE_SCORE isn't less than the previous score of $LIGHTHOUSE_MASTER_SCORE on the master branch"
-		PR_MESSAGE="Lighthouse test passed! The score of \`$LIGHTHOUSE_SCORE\` isn't less than the previous score of \`$LIGHTHOUSE_MASTER_SCORE\` on the master branch."
-		exit 0
-	fi
-
+if [ $LIGHTHOUSE_SCORE -lt $LIGHTHOUSE_MASTER_SCORE ]; then
+	# Lighthouse test failed! The score is less than the previous result on the master branch
+	echo -e "\nLighthouse test failed! The score of $LIGHTHOUSE_SCORE is less than the previous score of $LIGHTHOUSE_MASTER_SCORE on the master branch"
+	PR_MESSAGE="Lighthouse test failed! The score of \`$LIGHTHOUSE_SCORE\` is less than the previous score of \`$LIGHTHOUSE_MASTER_SCORE\` on the master branch."
+	exit 1
 else
-	# Lighthouse test passed! The score was recorded but there is no reference score on the master branch
-	echo -e "\nLighthouse test passed! The score of $LIGHTHOUSE_SCORE was recorded but there is not reference score for the master branch"
-	PR_MESSAGE="Lighthouse test passed! The score of \`$LIGHTHOUSE_SCORE\` was recorded but there is not reference score for the master branch."
+	# Lighthouse test passed! The score isn't less than the previous result on the master branch
+	echo -e "\nLighthouse test passed! The score of $LIGHTHOUSE_SCORE isn't less than the previous score of $LIGHTHOUSE_MASTER_SCORE on the master branch"
+	PR_MESSAGE="Lighthouse test passed! The score of \`$LIGHTHOUSE_SCORE\` isn't less than the previous score of \`$LIGHTHOUSE_MASTER_SCORE\` on the master branch."
+	exit 0
 fi
 
 PR_MESSAGE="$PR_MESSAGE View the full $REPORT_LINK"
 
 # Post the report back to the pull request on GitHub
-if [[ ${CIRCLE_BRANCH} != "master" ]]; then
-	echo -e "\nPosting Lighthouse results back to $LIGHTHOUSE_BRANCH "
-	curl -i -u "$GIT_USERNAME:$GIT_TOKEN" -d "{\"body\": \"$PR_MESSAGE\"}" $GITHUB_API_URL/issues/$PR_NUMBER/comments	
-fi
+echo -e "\nPosting Lighthouse results back to $LIGHTHOUSE_BRANCH "
+curl -i -u "$GIT_USERNAME:$GIT_TOKEN" -d "{\"body\": \"$PR_MESSAGE\"}" $GITHUB_API_URL/issues/$PR_NUMBER/comments

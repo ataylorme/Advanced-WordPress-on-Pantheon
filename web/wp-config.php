7bfb0063fcc1fe33f62945a9eca610f133019122
ataylorme <andrew@ataylor.me>
2018-06-06 10:20:26 -0700

    Don't allow an empty DB_PASSWORD

--- web/wp-config.php
+++ web/wp-config.php
@@ -84,7 +84,7 @@ define( 'WP_POST_REVISIONS', 3 );
  */
 define( 'DB_NAME', getenv( 'DB_NAME' ) );
 define( 'DB_USER', getenv( 'DB_USER' ) );
-define( 'DB_PASSWORD', getenv( 'DB_PASSWORD' ) !== false ? getenv( 'DB_PASSWORD' ) : '' );
+define( 'DB_PASSWORD', getenv( 'DB_PASSWORD' ) );
 define( 'DB_HOST', getenv( 'DB_HOST' ) );
 
 /**

3bd2a4ed60ba2433909a3956f39aca30a77584e3
ataylorme <andrew@ataylor.me>
2018-07-31 08:44:53 -0700

    Add rootPath

--- web/wp-content/themes/twentyseventeen-child/gulp/constants.js
+++ web/wp-content/themes/twentyseventeen-child/gulp/constants.js
@@ -5,19 +5,22 @@ export const reload = browserSync.reload();
 
 export const url = 'https://nginx/';
 
+// Root path is where npm run commands happen
+export const rootPath = process.env.INIT_CWD;
+
 export const dirs = {
-    src: 'source',
-    dest: 'assets'
+    src: `${rootPath}/source`,
+    dest: `${rootPath}/assets`
 };
 
 export const sassPaths = {
-    src: `${dirs.src}/css/`,
+    src: `${dirs.src}/css/**/*.scss`,
     mainFile: `${dirs.src}/css/twentyseventeen-child.scss`,
     dest: `${dirs.dest}/css/`
 };
 
 export const JSpaths = {
-    src: `${dirs.src}/js/`,
+    src: `${dirs.src}/js/**/*.js`,
     mainFile: `${dirs.src}/js/twentyseventeen-child.js`,
     dest: `${dirs.dest}/js/`
 };

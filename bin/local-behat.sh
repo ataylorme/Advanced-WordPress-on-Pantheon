df8d36210b5357c774cc19012db6f30bbc58a451
ataylorme <andrew@ataylor.me>
2018-10-02 16:30:23 -0700

    Run the local Behat script straight from Lando

--- bin/local-behat.sh
+++ bin/local-behat.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Create the WordPress admin user
-lando wp user delete admin --yes
-lando wp user create admin no-reply@pantheon.io --user_pass=admin --role=administrator
+wp user delete admin --yes
+wp user create admin no-reply@pantheon.io --user_pass=admin --role=administrator
 
 # Disable the lh-hsts plugin as it causes a redirect loop
-lando wp plugin deactivate lh-hsts
+wp plugin deactivate lh-hsts
 
 #Run WordHat
-lando wordhat
+./vendor/bin/behat --config=tests/behat/behat-lando.yml --strict
 
 # Re-enable the lh-hsts plugin
-lando wp plugin deactivate lh-hsts
+wp plugin deactivate lh-hsts
 
 # Remove the admin user
-lando wp user delete admin --yes
+wp user delete admin --yes

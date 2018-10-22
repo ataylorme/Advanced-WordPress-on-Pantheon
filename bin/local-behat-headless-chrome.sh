6999ae22c0b54869b44a8f6b7a6e248e980d5e1f
ataylorme <andrew@ataylor.me>
2018-10-22 14:40:00 -0700

    Move DB import and export to separate files

--- bin/local-behat-headless-chrome.sh
+++ bin/local-behat-headless-chrome.sh
@@ -12,34 +12,8 @@ else
     echo -e "\n Chrome started in headless mode ..."
 fi
 
-# Stash DB credentials
-DB_USER=pantheon
-DB_PASSWORD=pantheon
-DB_HOST=database
-DB_PORT=3306
-DB_NAME=pantheon
-DB_FILE_NAME="pre-behat-tests"
-
 # Export the DB
-
-# Remove old DB export files if they exists
-if [ -f $DB_FILE_NAME.sql ]
-then
-    rm $DB_FILE_NAME.sql
-fi
-
-if [ -f $DB_FILE_NAME.sql.gz ]
-then
-    rm $DB_FILE_NAME.sql.gz
-fi
-
-# Dump the current DB
-echo -e "\nExporting the $DB_NAME database to $DB_FILE_NAME.sql ..."
-mysqldump --opt --user=$DB_USER --host=$DB_HOST --port=$DB_PORT --password=$DB_PASSWORD $DB_NAME > $DB_FILE_NAME.sql
-
-# gzip the backup file
-echo -e "\nGzipping $DB_FILE_NAME.sql ..."
-gzip $DB_FILE_NAME.sql
+./bin/export-db.sh
 
 # Create the WordPress admin user
 echo -e "\nCreating the WordPress admin user ..."
@@ -53,29 +27,4 @@ wp plugin deactivate lh-hsts
 ./vendor/bin/behat --config=tests/behat/behat-lando.yml --strict
 
 # Restore the DB
-
-# Un-gzip the backup
-gunzip $DB_FILE_NAME.sql.gz
-
-# Build the SQL prefix
-SQLSTART="mysql --user=$DB_USER --host=$DB_HOST --port=$DB_PORT --password=$DB_PASSWORD $DB_NAME"
-
-# Get all the tables
-DB_TABLES=$($SQLSTART -e 'SHOW TABLES' | awk '{ print $1}' | grep -v '^Tables' )
-
-# Drop each tables
-echo -e "\nDropping all the tables in the $DB_NAME database..."
-for DB_TABLE in $DB_TABLES; do
-    $SQLSTART -e "DROP TABLE $DB_TABLE"
-done
-
-# Import the DB
-echo -e "\nImporting the $DB_NAME database..."
-cat $DB_FILE_NAME.sql | $SQLSTART
-
-# Remove the DB export file
-echo -e "\nCleaning up the leftover $DB_FILE_NAME.sql ..."
-if [ -f $DB_FILE_NAME.sql ]
-then
-    rm $DB_FILE_NAME.sql
-fi
+#./bin/restore-db.sh
